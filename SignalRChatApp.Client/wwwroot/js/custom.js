9d9dc9ecd99898059b12d054c36d0bd1c28ed307
batuhanb <balibatuhan19@gmail.com>
2023-10-18 13:39:24 +0300

    reconnect-onconnect-closed actions added

--- SignalRChatApp.Client/wwwroot/js/custom.js
+++ SignalRChatApp.Client/wwwroot/js/custom.js
@@ -1,8 +1,53 @@
 $(document).ready(() => {
     var conn = new signalR.HubConnectionBuilder().
-        withUrl("https://localhost:7270/chat-hub").build();
+        withUrl("https://localhost:7270/chat-hub")
+        .withAutomaticReconnect([1000,1000,4000,8000]) // in normally tries in 0 - 2 - 10 - 30 seconds
+        .build();
 
-    conn.start();
+    async function start() {
+        try {
+            await conn.start();
+        } catch (e) {
+            setTimeout(() => start(), 2000);
+        }
+    }
+
+    /*conn.start();*/
+    start();
+
+    const status = $("#conn-result");
+    conn.onreconnecting(error => {
+        status.css("background-color", "blue");
+        status.css("color", "white");
+        status.html("Connecting...");
+        status.fadeIn(2000, () => {
+            setTimeout(() => {
+                status.fadeOut(2000);
+            }, 2000);
+        })
+    });
+
+    conn.onreconnected(connId => {
+        status.css("background-color", "green");
+        status.css("color", "white");
+        status.html("Connected!");
+        status.fadeIn(2000, () => {
+            setTimeout(() => {
+                status.fadeOut(2000);
+            }, 2000);
+        })
+    });
+
+    conn.onclose(connId => {
+        status.css("background-color", "red");
+        status.css("color", "white");
+        status.html("Connection Error!");
+        status.fadeIn(2000, () => {
+            setTimeout(() => {
+                status.fadeOut(2000);
+            }, 2000);
+        })
+    });
 
     $("#btn-send").click(() => {
         let message = $("#txt-message").val();
@@ -13,4 +58,6 @@ $(document).ready(() => {
     conn.on("receiveMessage",function( user,message ){
         $("#result").append(user + " says " + message + "<br>");
     })
+
+    
 });
