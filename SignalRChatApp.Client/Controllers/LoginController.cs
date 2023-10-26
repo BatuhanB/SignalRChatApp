b1ecfb153459fce4a012ecd1bb85ea4339198412
batuhanb <balibatuhan19@gmail.com>
2023-10-26 20:04:01 +0300

    Register action tested and inputs works fine

--- SignalRChatApp.Client/Controllers/LoginController.cs
+++ SignalRChatApp.Client/Controllers/LoginController.cs
@@ -1,10 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SignalRChatApp.Client.Models;
 
 namespace SignalRChatApp.Client.Controllers
 {
     public class LoginController : Controller
     {
+        // Index Page
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["FirstName"] = model.FirstName;
+                ViewData["LastName"] = model.LastName;
+                ViewData["UserName"] = model.UserName;
+                ViewData["Email"] = model.Email;
+                ViewData["Password"] = model.Password;
+                return View("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Login()
         {
             return View();
         }
