3e8946b91e687c6983095a5e822f1ad433c0a79d
batuhanb <balibatuhan19@gmail.com>
2023-11-28 21:32:54 +0300

    Added strongly typed hubs

--- SignalRChatApp.Api/Hubs/ChatHub.cs
+++ SignalRChatApp.Api/Hubs/ChatHub.cs
@@ -1,28 +1,34 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRChatApp.Api.Interfaces;
 
 namespace SignalRChatApp.Api.Hubs
 {
-    public class ChatHub : Hub
+    public class ChatHub : Hub<IMessageClient>
     {
         static List<string> clients = new();
 
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("receiveMessage", user, message);
+            //await Clients.All.SendAsync("receiveMessage", user, message);
+            await Clients.All.SendMessage(user, message);
         }
 
         public async override Task OnConnectedAsync()
         {
             clients.Add(Context.ConnectionId);
-            await Clients.All.SendAsync("clients", clients);
-            await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+            //await Clients.All.SendAsync("clients", clients);
+            //await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+            await Clients.All.Clients(clients);
+            await Clients.All.UserConnected(Context.ConnectionId);
         }
 
         public async override Task OnDisconnectedAsync(Exception? exception)
         {
             clients.Remove(Context.ConnectionId);
-            await Clients.All.SendAsync("clients", clients);
-            await Clients.All.SendAsync("userLeft", Context.ConnectionId);
+            //await Clients.All.SendAsync("clients", clients);
+            //await Clients.All.SendAsync("userLeft", Context.ConnectionId);
+            await Clients.All.Clients(clients);
+            await Clients.All.UserDisconnected(Context.ConnectionId);
         }
     }
 }
---
+++ SignalRChatApp.Api/Interfaces/IMessageClient.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRChatApp.Api.Interfaces
+{
+    public interface IMessageClient
+    {
+        Task SendMessage(string user,string message);
+        Task Clients(List<string> clients);
+        Task UserConnected(string connectionId);
+        Task UserDisconnected(string connectionId);
+    }
+}
