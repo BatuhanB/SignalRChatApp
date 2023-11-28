293e5c15df374c1dfb477c2a3555adf08e016398
batuhanb <balibatuhan19@gmail.com>
2023-11-28 20:49:05 +0300

    print all the connId who connected or disconnected from hub

--- SignalRChatApp.Api/Hubs/ChatHub.cs
+++ SignalRChatApp.Api/Hubs/ChatHub.cs
@@ -4,9 +4,25 @@ namespace SignalRChatApp.Api.Hubs
 {
     public class ChatHub : Hub
     {
+        static List<string> clients = new();
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("receiveMessage", user, message);
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+        }
+
+        public async override Task OnDisconnectedAsync(Exception? exception)
+        {
+            clients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userLeft", Context.ConnectionId);
         }
     }
 }
